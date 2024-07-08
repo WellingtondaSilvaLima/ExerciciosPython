330623e418337d4fdfc3a6afc5dba6467ec189ae
Wellington Lima <wellingtonlima.jt@gmail.com>
2024-07-08 10:20:20 -0300

    Início das funcionalidade

--- calculadora/app.py
+++ calculadora/app.py
@@ -5,7 +5,7 @@ from tkinter import *
 # Configuração da tela
 janela = Tk()
 janela.title('Calculathor')
-janela.geometry('494x600')
+janela.geometry('494x282')
 janela.resizable(False, False)
 
 # Display
@@ -20,7 +20,7 @@ resultado_na_tela = Label(janela,
                           anchor='e',
                           font=('Verdana', 12, 'italic'),
                           width=43)
-resultado_na_tela.grid(row=0, column=0, columnspan=6,)
+resultado_na_tela.grid(row=0, column=0, columnspan=5,)
 
 ## Teclas
 # Fórmulas #
@@ -30,31 +30,96 @@ class Botao:
     self.linha = linha
     self.coluna = coluna
 
-  def cria_botao(self):
+  def cria_botao(self, comando, mescla=False):
     btn_generico = Button(janela,
                           text=self.texto,
                           padx=10,
                           pady=10,
                           bd=2,
                           font=('Verdana', 10),
-                          relief=GROOVE)
-    btn_generico.grid(row=self.linha, column=self.coluna, sticky='nsew')
+                          relief=GROOVE,
+                          command=comando)
+    self.__mescla_linhas(btn_generico, mescla)
+
+  def __mescla_linhas(self, btn_para_mesclar, mescla:bool):  
+    if mescla == False:
+      btn_para_mesclar.grid(row=self.linha, column=self.coluna, sticky='nsew')
+    else:
+      btn_para_mesclar.grid(row=self.linha, column=self.coluna, sticky='nsew', rowspan=2)
 
 
 btn_seno = Botao('Sin', 1, 0)
-btn_seno.cria_botao()
+btn_seno.cria_botao(lambda: seno())
 
 btn_cosseno = Botao('Cos', 1, 1)
-btn_cosseno.cria_botao()
+btn_cosseno.cria_botao(lambda: cosseno())
 
 btn_tangente = Botao('Tan', 1, 2)
-btn_tangente.cria_botao()
+btn_tangente.cria_botao(lambda: tangente())
 
-btn_raiz_nesima = Botao('x^n', 1, 3)
-btn_raiz_nesima.cria_botao()
+btn_raiz_nesima = Botao('x^1/n', 1, 3)
+btn_raiz_nesima.cria_botao(lambda: raiz_nesima())
 
-btn_raiz_quadrada = Botao('x^2', 1, 4)
-btn_raiz_quadrada.cria_botao()
+btn_raiz_quadrada = Botao('x^1/2', 2, 4)
+btn_raiz_quadrada.cria_botao(lambda: raiz_quadrada())
+
+btn_limpar = Botao('C', 1, 4)
+btn_limpar.cria_botao(lambda: limpa_visor(resultado_na_tela))
+
+btn_potencia = Botao('x^n', 3, 4)
+btn_potencia.cria_botao(lambda: exponenciacao())
+
+btn_sete = Botao('7', 2, 0)
+btn_sete.cria_botao(lambda: mostra_numeros(7, resultado_na_tela))
+
+btn_oito = Botao('8', 2, 1)
+btn_oito.cria_botao(lambda: mostra_numeros(8, resultado_na_tela))
+
+btn_nove = Botao('9', 2, 2)
+btn_nove.cria_botao(lambda: mostra_numeros(9, resultado_na_tela))
+
+btn_divisao = Botao('/', 2, 3)
+btn_divisao.cria_botao(lambda: divisao())
+
+btn_multiplicacao = Botao('*', 3, 3)
+btn_multiplicacao.cria_botao(lambda: multiplicacao())
+
+btn_quatro = Botao('4', 3, 0)
+btn_quatro.cria_botao(lambda: mostra_numeros(4, resultado_na_tela))
+
+btn_cinco = Botao('5', 3, 1)
+btn_cinco.cria_botao(lambda: mostra_numeros(5, resultado_na_tela))
+
+btn_seis = Botao('6', 3, 2)
+btn_seis.cria_botao(lambda: mostra_numeros(6, resultado_na_tela))
+
+btn_subtracao = Botao('-', 4, 3)
+btn_subtracao.cria_botao(lambda: subtracao())
+
+btn_um = Botao('1', 4, 0)
+btn_um.cria_botao(lambda: mostra_numeros(1, resultado_na_tela))
+
+btn_dois = Botao('2', 4, 1)
+btn_dois.cria_botao(lambda: mostra_numeros(2, resultado_na_tela))
+
+btn_tres = Botao('3', 4, 2)
+btn_tres.cria_botao(lambda: mostra_numeros(3, resultado_na_tela))
+
+btn_soma = Botao('+', 5, 3)
+btn_soma.cria_botao(lambda: soma())
+
+btn_igual = Botao('=', 4, 4)
+btn_igual.cria_botao(lambda: igual(), True)
+
+btn_ponto = Botao('.', 5, 0)
+btn_ponto.cria_botao(lambda: ponto_para_virgula())
+
+btn_zero = Botao('0', 5, 1)
+btn_zero.cria_botao(lambda: mostra_numeros(0, resultado_na_tela))
+
+btn_virgula = Botao(',', 5, 2)
+btn_virgula.cria_botao(lambda: virgula_para_ponto())
+
 
 
 janela.mainloop()
--- calculadora/funcoes.py
+++ calculadora/funcoes.py
@@ -1,38 +1,126 @@
 from math import sqrt, sin, cos, tan
+from tkinter import Label
 
 
-def soma(valor1, valor2):
+def soma(valor1=0, valor2=0):
   return valor1 + valor2
 
-def subtracao(valor1, valor2):
+def subtracao(valor1=0, valor2=0):
   return valor1 - valor2
 
-def multiplicacao(valor1, valor2):
+def multiplicacao(valor1=0, valor2=0):
   return valor1 * valor2
 
-def divisao(valor1, valor2):
+def divisao(valor1=0, valor2=1):
   return valor1 / valor2
 
-def exponenciacao(valor, expoente):
+def exponenciacao(valor=0, expoente=1):
   return valor**expoente
 
-def raiz_quadrada(valor):
+def raiz_quadrada(valor=1):
   return sqrt(valor)
 
-def raiz_nesima(valor, indice):
+def raiz_nesima(valor=0, indice=1):
   return valor ** (1/indice)
 
-def seno(valor):
+def seno(valor=0):
   return sin(valor)
 
-def cosseno(valor):
+def cosseno(valor=0):
   return cos(valor)
 
-def tangente(valor):
+def tangente(valor=0):
   return tan(valor)
 
-def mudar_sinal(valor):
+def mudar_sinal(valor=1):
   return -(valor)
+
+def limpa_visor(visor:Label):
+  visor.config(text='0')
+
+def mostra_numeros(numero:int, visor:Label):
+  match str(numero):
+    case '0':
+      texto_atual = visor.cget('text')
+      if texto_atual == '0':
+        visor['text'] = '0'
+      else:
+        texto_novo = texto_atual + '0'
+        visor['text'] = texto_novo
+    case '1':
+      texto_atual = visor.cget('text')
+      if texto_atual == '0':
+        visor['text'] = '1'
+      else:
+        texto_novo = texto_atual + '1'
+        visor['text'] = texto_novo
+    case '2':
+      texto_atual = visor.cget('text')
+      if texto_atual == '0':
+        visor['text'] = '2'
+      else:
+        texto_novo = texto_atual + '2'
+        visor['text'] = texto_novo
+    case '3':
+      texto_atual = visor.cget('text')
+      if texto_atual == '0':
+        visor['text'] = '3'
+      else:
+        texto_novo = texto_atual + '3'
+        visor['text'] = texto_novo
+    case '4':
+      texto_atual = visor.cget('text')
+      if texto_atual == '0':
+        visor['text'] = '4'
+      else:
+        texto_novo = texto_atual + '4'
+        visor['text'] = texto_novo
+    case '5':
+      texto_atual = visor.cget('text')
+      if texto_atual == '0':
+        visor['text'] = '5'
+      else:
+        texto_novo = texto_atual + '5'
+        visor['text'] = texto_novo
+    case '6':
+      texto_atual = visor.cget('text')
+      if texto_atual == '0':
+        visor['text'] = '6'
+      else:
+        texto_novo = texto_atual + '6'
+        visor['text'] = texto_novo
+    case '7':
+      texto_atual = visor.cget('text')
+      if texto_atual == '0':
+        visor['text'] = '7'
+      else:
+        texto_novo = texto_atual + '7'
+        visor['text'] = texto_novo
+    case '8':
+      texto_atual = visor.cget('text')
+      if texto_atual == '0':
+        visor['text'] = '8'
+      else:
+        texto_novo = texto_atual + '8'
+        visor['text'] = texto_novo
+    case '9':
+      texto_atual = visor.cget('text')
+      if texto_atual == '0':
+        visor['text'] = '9'
+      else:
+        texto_novo = texto_atual + '9'
+        visor['text'] = texto_novo
+    case _:
+      visor['text'] = 'Error'
+
+def igual():
+  pass
+
+def ponto_para_virgula():
+  pass
+
+def virgula_para_ponto():
+  pass
 
 
 # SDG
